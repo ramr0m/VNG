dc7e2dd5081f4d30570e3f177bc7ee8b2bfa1b45
Ramon <git@ramonstouten.nl>
2017-09-17 21:47:55 +0200

    Initial add

---
+++ VNG/Implementations/InputModuleDISK.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VNG.Implementations.Input
+{
+    public class InputModuleDISK : Interfaces.Input.IInputModule
+    {
+        public void Map()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
